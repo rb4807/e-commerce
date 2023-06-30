18fa87f22d4458d5048ea5f86c910885eb2558af
unknown <rb160601@gmail.com>
2023-06-30 14:22:24 +0530

    initial commit

---
+++ base/views.py
@@ -0,0 +1,234 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'base/index.html')
+
+def oneplus(request):
+    return render(request, 'base/oneplus.html')
+
+def iphone(request):
+    return render(request, 'base/iphone.html')
+
+def sam_double_door(request):
+    return render(request, 'base/sam_double_door.html')
+
+def sam_single_door(request):
+    return render(request, 'base/sam_single_door.html')
+
+def whirl_triple_door(request):
+    return render(request, 'base/whirl_triple_door.html')
+
+def sam_frost_free(request):
+    return render(request, 'base/sam_frost_free.html')
+
+def lg_side_by_side(request):
+    return render(request, 'base/lg_side_by_side.html')
+
+def ac(request):
+    return render(request, 'base/ac.html')
+
+def account(request):
+    return render(request, 'base/account.html')
+
+def alexa(request):
+    return render(request, 'base/alexa.html')
+
+def apple(request):
+    return render(request, 'base/apple.html')
+
+def bajaj_oven(request):
+    return render(request, 'base/bajaj_oven.html')
+
+def boat(request):
+    return render(request, 'base/boat.html')
+
+def boAt_smartwatch(request):
+    return render(request, 'base/boAt_smartwatch.html')
+
+def bosch_mixer(request):
+    return render(request, 'base/bosch_mixer.html')
+
+def bosch_washing(request):
+    return render(request, 'base/bosch_washing.html')
+
+def brush(request):
+    return render(request, 'base/brush.html')
+
+def butterfly_mixer(request):
+    return render(request, 'base/butterfly_mixer.html')
+
+def realme(request):
+    return render(request, 'base/realme.html')
+
+def canon(request):
+    return render(request, 'base/canon.html')
+
+def cart(request):
+    return render(request, 'base/cart.html')
+
+def combo(request):
+    return render(request, 'base/combo.html')
+
+def dell(request):
+    return render(request, 'base/dell.html')
+
+def fridge(request):
+    return render(request, 'base/fridge.html')
+
+def dev(request):
+    return render(request, 'base/dev.html')
+
+def moto(request):
+    return render(request, 'base/moto.html')
+
+def haier_ac(request):
+    return render(request, 'base/haier_ac.html')
+
+def headphone(request):
+    return render(request, 'base/headphone.html')
+
+def ifb_oven(request):
+    return render(request, 'base/ifb_oven.html')
+
+def ifb_washing(request):
+    return render(request, 'base/ifb_washing.html')
+
+def lakshmi_top(request):
+    return render(request, 'base/lakshmi_top.html')
+
+def lg_ac(request):
+    return render(request, 'base/lg_ac.html')
+
+def lg_oven(request):
+    return render(request, 'base/lg_oven.html')
+
+def lg_tv(request):
+    return render(request, 'base/lg_tv.html')
+
+def lg_washing(request):
+    return render(request, 'base/lg_washing.html')
+
+def lloyd_ac(request):
+    return render(request, 'base/lloyd_ac.html')
+
+def mi_camera(request):
+    return render(request, 'base/mi_camera.html')
+
+def mi_tv(request):
+    return render(request, 'base/mi_tv.html')
+
+def mixer(request):
+    return render(request, 'base/mixer.html')
+
+def monitor(request):
+    return render(request, 'base/monitor.html')
+
+def mouse(request):
+    return render(request, 'base/mouse.html')
+
+def nikon(request):
+    return render(request, 'base/nikon.html')
+
+def oneplus_tv(request):
+    return render(request, 'base/oneplus_tv.html')
+
+def payment(request):
+    return render(request, 'base/payment.html')
+
+def pendrive(request):
+    return render(request, 'base/pendrive.html')
+
+def pigeon_mixer(request):
+    return render(request, 'base/pigeon_mixer.html')
+
+def pigeon_top(request):
+    return render(request, 'base/pigeon_top.html')
+
+def power_bank(request):
+    return render(request, 'base/power_bank.html')
+
+def preethi_top(request):
+    return render(request, 'base/preethi_top.html')
+
+def router(request):
+    return render(request, 'base/router.html')
+
+def s23(request):
+    return render(request, 'base/s23.html')
+
+def sam_ac(request):
+    return render(request, 'base/sam_ac.html')
+
+def sam_led(request):
+    return render(request, 'base/sam_led.html')
+
+def sam_ips(request):
+    return render(request, 'base/sam_ips.html')
+
+def samsung_oven(request):
+    return render(request, 'base/samsung_oven.html')
+
+def samsung_washing(request):
+    return render(request, 'base/samsung_washing.html')
+
+def samsung_tv(request):
+    return render(request, 'base/samsung_tv.html')
+
+def samsung_washing(request):
+    return render(request, 'base/samsung_washing.html')
+
+def sony_tv(request):
+    return render(request, 'base/sony_tv.html')
+
+def successful(request):
+    return render(request, 'base/successful.html'
+                  )
+def vivo(request):
+    return render(request, 'base/vivo.html')
+
+def table_top(request):
+    return render(request, 'base/table_top.html')
+
+def tv(request):
+    return render(request, 'base/tv.html')
+
+def usha_mixer(request):
+    return render(request, 'base/usha_mixer.html')
+
+def washing_machine(request):
+    return render(request, 'base/washing_machine.html')
+
+def whirlpool_ac(request):
+    return render(request, 'base/whirlpool_ac.html')
+
+def whirlpool_washing(request):
+    return render(request, 'base/whirlpool_washing.html')
+
+def poco(request):
+    return render(request, 'base/poco.html')
+
+def oven(request):
+    return render(request, 'base/oven.html')
+
+def lg_double_door(request):
+    return render(request, 'base/lg_double_door.html')
+
+def prestige_mixer(request):
+    return render(request, 'base/prestige_mixer.html')
+
+def panasonic_oven(request):
+    return render(request, 'base/panasonic_oven.html')
+
+def butterfly_top(request):
+    return render(request, 'base/butterfly_top.html')
+
+def prestige_top(request):
+    return render(request, 'base/prestige_top.html')
+
+def sam_ips(request):
+    return render(request, 'base/sam_ips.html')
+
+def sam_led(request):
+    return render(request, 'base/sam_led.html')
